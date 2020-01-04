13c18a8a364856c190db07308c598fc370c21c0e
MirijamTrippel <59499301+MirijamTrippel@users.noreply.github.com>
2020-01-04 13:59:26 +0100

    Für die Gruppenarbeit

---
+++ project/adaptierung/exercise10-master/your-zuul/Room.java
@@ -0,0 +1,92 @@
+/**
+ * Class Room - a room in an adventure game.
+ *
+ * This class is part of the "World of Zuul" application. 
+ * "World of Zuul" is a very simple, text based adventure game.  
+ *
+ * A "Room" represents one location in the scenery of the game.  It is 
+ * connected to other rooms via exits.  The exits are labelled north, 
+ * east, south, west.  For each direction, the room stores a reference
+ * to the neighboring room, or null if there is no exit in that direction.
+ * 
+ * @author  Michael Kölling and David J. Barnes
+ * @version 2016.02.29
+ */
+public class Room 
+{
+    public static String description;
+    public static Room northExit;
+    public static Room southExit;
+    public static Room eastExit;
+    public static Room westExit;
+    
+    public Room currentRoom;
+
+    /**
+     * Create a room described "description". Initially, it has
+     * no exits. "description" is something like "a kitchen" or
+     * "an open court yard".
+     * @param description The room's description.
+     */
+    public Room(){
+        createRooms();
+    }
+    public Room(String description) 
+    {
+        this.description = description;
+    }
+
+    /**
+     * Define the exits of this room.  Every direction either leads
+     * to another room or is null (no exit there).
+     * @param north The north exit.
+     * @param east The east east.
+     * @param south The south exit.
+     * @param west The west exit.
+     */
+    public void setExits(Room north, Room east, Room south, Room west) 
+    {
+        if(north != null) {
+            northExit = north;
+        }
+        if(east != null) {
+            eastExit = east;
+        }
+        if(south != null) {
+            southExit = south;
+        }
+        if(west != null) {
+            westExit = west;
+        }
+    }
+
+    /**
+     * @return The description of the room.
+     */
+    public static String getDescription()
+    {
+        return description;
+    }
+
+    public void createRooms()
+    {
+        Room outside, theater, pub, lab, office;
+
+        // create the rooms
+        outside = new Room("outside the main entrance of the university");
+        theater = new Room("in a lecture theater");
+        pub = new Room("in the campus pub");
+        lab = new Room("in a computing lab");
+        office = new Room("in the computing admin office");
+
+        // initialise room exits
+        outside.setExits(null, theater, lab, pub);
+        theater.setExits(null, null, null, outside);
+        pub.setExits(null, outside, null, null);
+        lab.setExits(outside, office, null, null);
+        office.setExits(null, null, null, lab);
+
+         currentRoom = outside;  // start game outside
+    }
+    
+}
